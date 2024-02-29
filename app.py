255947bd242efce08e1f7e548809936ca63da1a2
Charles Plessy <charles.plessy@oist.jp>
2024-02-29 10:33:47 +0900

    Working proof of principle.

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ ssh_user = ssh_config['ssh_user']
 ssh_server = ssh_config['ssh_server']
 ssh_key_path = ssh_config['ssh_key_path']
 ssh_command_prefix = f"ssh -t -i {ssh_key_path} {ssh_user}@{ssh_server}"
+nextflow_command = "PATH=$PATH:/apps/free81/singularity/3.5.2/bin JAVA_HOME=/apps/free81/java-jdk/14 /apps/unit/BioinfoUgrp/Other/Nextflow2/23.10.1/bin/nextflow"
+nextflow_pipeline = "oist/plessy_pairwiseGenomeComparison -r o2m"
+nextflow_profile = "-profile oist"
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_sequence():
@@ -33,38 +36,36 @@ def upload_sequence():
         sequence = request.form['sequence']
 
         # Generate a unique filename to avoid conflicts
-        filename = f"{uuid.uuid4()}.txt"
+        session_id = f"{uuid.uuid4()}"
+        filename = f"{session_id}.txt"
+        outdir = f"{session_id}_results"
         local_path = os.path.join('/tmp', filename)
 
         with open(local_path, 'w') as file:
             file.write(sequence)
-        logging.info('Wrote file…')
 
         # Define the remote path
         remote_path = '/flash/LuscombeU/charles-plessy/cache/last-flask'
-        remote_file = f"{remote_path}{filename}"
+        remote_file = f"{remote_path}/{filename}"
+        remote_outdir = f"{remote_path}/{outdir}"
 
         # Ensure the remote directory exists
         mkdir_command = f"{ssh_command_prefix} 'mkdir -p {remote_path}'"
-        logging.info(f"Want to run {mkdir_command}")
         subprocess.run(mkdir_command, shell=True)
-        logging.info('Ensured destination directory is there…')
 
         # SCP to transfer the file to the compute server
         scp_command = f"scp -i {ssh_key_path} {local_path} {ssh_user}@{ssh_server}:{remote_file}"
         subprocess.run(scp_command, shell=True)
-        logging.info('Transferred file…')
 
         # SSH to execute the command on the compute server
-        command_on_server = f"{ssh_command_prefix} '/apps/unit/BioinfoUgrp/Other/Nextflow2/23.10.1/bin/nextflow run oist/plessy_pairwiseGenomeComparison -profile oist --target /bucket/.deigo/LuscombeU/common/Oikopleura/Genomes/OKI2018_I69_1.0/zenodo_1.1 -w {remote_path}/work --query {remote_file}'"
+        command_on_server = f"{ssh_command_prefix} '{nextflow_command} run {nextflow_pipeline} {nextflow_profile} --target /bucket/.deigo/LuscombeU/common/Oikopleura/Genomes/OKI2018_I69_1.0/zenodo_1.1 -w {remote_path}/work --query {remote_file}' --outdir {remote_outdir} --lastal_params /flash/LuscombeU/charles-plessy/cache/last-flask/OKI2018_I69_1.0.target.par -resume --m2m --skip_dotplot_m2m --skip_dotplot_m2o --skip_dotplot_o2m --skip_dotplot_o2o"
         logging.info(f"Want to run {command_on_server}")
         subprocess.run(command_on_server, shell=True)
-        logging.info('Ran Nextflow…')
 
         # SCP to retrieve the gzipped result
-        result_filename_gz = f"results/last/query.01.m2m_aln.maf.gz"
+        result_filename_gz = f"query.01.m2m_aln.maf.gz"
         result_local_path_gz = os.path.join('/tmp', result_filename_gz)
-        result_remote_file_gz = f"{remote_path}{result_filename_gz}"
+        result_remote_file_gz = f"{remote_outdir}/last/{result_filename_gz}"
         scp_command_result = f"scp -i {ssh_key_path} {ssh_user}@{ssh_server}:{result_remote_file_gz} {result_local_path_gz}"
         subprocess.run(scp_command_result, shell=True)
         logging.info('Got the results back…')
@@ -92,7 +93,7 @@ def upload_sequence():
     return '''
     <!doctype html>
     <title>Upload Sequence</title>
-    <h1>Upload a sequence</h1>
+    <h1>Upload a sequence in FASTA format (do not forget the header).</h1>
     <form method=post>
       <textarea name=sequence></textarea>
       <input type=submit value=Upload>
